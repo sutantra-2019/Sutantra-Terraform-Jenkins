3e255f37d67988a2fadd228e8e3fef114558a255
Arun Kumar Chunduu <>
2020-12-29 16:05:16 -0500

    jenkins

--- main.tf
+++ main.tf
@@ -28,4 +28,6 @@ resource "aws_s3_bucket_public_access_block" "s3_public_access" {
   block_public_policy     = var.block_public_policy
   ignore_public_acls      = var.ignore_public_acls
   restrict_public_buckets = var.restrict_public_buckets
+
+  depends_on = [aws_s3_bucket.Terraform-Bucket]
 }
